96b28d4910d59dc277cbb5e9c9e7537c352603a1
Karimka01 <92387966+Karimka01@users.noreply.github.com>
2021-11-09 19:38:06 +0300

    Add files via upload

---
+++ Archer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08._06._21
+{  
+   public enum HeroClass
+   {
+       Archer
+   }
+    class Hero : Bow
+    {
+        public string name { get=>"Jonh"; private set { } }
+        public int strenght { get; set; }
+        public string description { get => "Black hair, Red eyes."; private set { } }
+        public HeroClass heroClass { get; private set; }
+        public Hero()
+        {
+            heroClass = HeroClass.Archer;
+        }
+        public string Jump()
+        {
+            return "*Очень высоко прыгает~*";
+        }
+        public string Run()
+        {
+            return "*Не может нормально бежать, без остановки прыгает*";
+        }
+        public string Stand()
+        {
+            return "*Прыгает на месте*";
+        }
+        public string Swim()
+        {
+            return "*Плывёт и бултыхается в верх одноврменно*";
+        }
+    }
+    class Bow
+    {
+        public int CritChance { get ; set; }
+        public int RangeDamage { get ; set; }
+    }
+
+}
+
+
+
+        
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _08._06._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero archer = new Hero();
+            Console.WriteLine("Сила персонажа: ");
+            archer.strenght = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Дальность лука: ");
+            archer.RangeDamage = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Крит лука: ");
+            archer.CritChance = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("      Персонаж");
+            Console.WriteLine("Имя: "+archer.name);
+            Console.WriteLine("Описание: " + archer.description);
+            Console.WriteLine("Класс: " + archer.heroClass);
+            Console.WriteLine("Сила атаки: " + archer.strenght);
+            Console.WriteLine("        Лук");
+            Console.WriteLine("Дальность атаки: " + archer.RangeDamage);
+            Console.WriteLine("Шанс крит удара: " + archer.CritChance);
+
+            bool flag = true;
+            string value = " ";
+            while (flag)
+            {
+                Console.WriteLine("Выберите действие(Jump,Run,Stand,Swim):");
+                string name = Console.ReadLine();
+                value = name;
+                switch (value)
+                {
+                    case "Jump":
+                        Console.WriteLine(archer.Jump());
+                        break;
+                    case "Run":
+                        Console.WriteLine(archer.Run());
+                        break;
+                    case "Stand":
+                        Console.WriteLine(archer.Stand());
+                        break;
+                    case "Swim":
+                        Console.WriteLine(archer.Swim());
+                        break;
+                }
+            }
+        }
+    }
+}
+
